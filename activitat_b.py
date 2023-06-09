4e2f48a04e23a15f9b78940aa43e1539a2994a79
mrodriguezzsx <mrodrigu@jaumebalmes.net>
2023-06-09 13:50:02 +0200

    Exercici B Fet

---
+++ activitat_b.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def totalPoblacio():
+    # Guardem les dades de l'excel en una variable
+    dades = pd.read_csv("activitat_b.csv", usecols=['City', 'Population'])
+
+    # Eliminem la linae buida de l'arxiu
+    dades = dades.dropna()
+
+    # Funcio per a que hagafi 10 resultats aleatoris
+    dades = dades.sample(10)
+    print(dades)
+
+    # Eliminem les comes de les poblacions ja que sino conta que son decimals
+    dades["Population"] = dades["Population"].apply(lambda x : x.replace(',',''))
+
+    plt.pie(dades["Population"], labels=dades['City'], autopct='%1.1f%%')
+    plt.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
+    return plt.show()
+
+
+def densitatKM2():
+    # Guardem les dades de l'excel en una variable
+    dades = pd.read_csv("activitat_b.csv", usecols=['City', 'Area KM2'])
+
+    # Eliminem la linae buida de l'arxiu
+    dades = dades.dropna()
+
+    # Funcio per a que hagafi 10 resultats aleatoris
+    dades = dades.sample(10)
+    print(dades)
+
+    plt.pie(dades["Area KM2"], labels=dades['City'], autopct='%1.1f%%')
+    plt.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
+    return plt.show()
+
+def densitatM2():
+    # Guardem les dades de l'excel en una variable
+    dades = pd.read_csv("activitat_b.csv", usecols=['City', 'Area   M2'])
+
+    # Eliminem la linae buida de l'arxiu
+    dades = dades.dropna()
+
+    # Funcio per a que hagafi 10 resultats aleatoris
+    dades = dades.sample(10)
+
+    print(dades)
+
+    plt.pie(dades["Area   M2"], labels=dades['City'], autopct='%1.1f%%')
+    plt.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
+    return plt.show()
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import activitat_b
+
+
+def activarFuncionsB():
+    activitat_b.totalPoblacio()
+    activitat_b.densitatKM2()
+    activitat_b.densitatM2()
+
+activarFuncionsB()
 
